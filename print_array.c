43ae5a1826cac26738ce79d449f74504b9605aaf
dominic-source <chinonsodomnic@gmail.com>
2023-07-21 20:43:51 +0000

    correct some bugs

--- print_array.c
+++ print_array.c
@@ -3,14 +3,14 @@
 
 /**
  * print_array - Prints an array of integers
- * 
+ *
  * @array: The array to be printed
  * @size: Number of elements in @array
  */
 void print_array(const int *array, size_t size)
 {
 	size_t i;
-	
+
 	i = 0;
 	while (array && i < size)
 	{
--- print_list.c
+++ print_list.c
@@ -9,7 +9,7 @@
 void print_list(const listint_t *list)
 {
 	int i;
-	
+
 	i = 0;
 	while (list)
 	{
