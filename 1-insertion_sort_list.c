5e794d4e63422fc6b60be813a1cc1a96c8205771
Obinna Ukwa <obikelv@gmail.com>
2023-07-22 07:58:16 -0700

    insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *inter, *insert, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (inter = (*list)->next; inter != NULL; inter = temp)
+	{
+		temp = inter->next;
+		insert = inter->prev;
+		while (insert != NULL && inter->n < insert->n)
+		{
+			swap_node(list, &insert, inter);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
+
+/**
+ * swap_node - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_node(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
--- sort.h
+++ sort.h
@@ -25,6 +25,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
+void swap_node(listint_t **h, listint_t **n1, listint_t *n2);
 
 #endif
 
